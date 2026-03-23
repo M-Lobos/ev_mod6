cac6b3e7af64dc99be0b41f0088253ba517c79d1
M-Lobos <manuel.lobos.88@gmail.com>
2026-03-22 21:01:48 -0300

    FEAT:  model.local.ZooDataBase implementation of the local DB entity

--- app/src/main/java/com/lobosmanuel/zoo_app/model/local/ZooDataBase.kt
+++ app/src/main/java/com/lobosmanuel/zoo_app/model/local/ZooDataBase.kt
@@ -1,10 +1,35 @@
 package com.lobosmanuel.zoo_app.model.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.lobosmanuel.zoo_app.model.remote.ZooAnimalData
 
 @Database(entities = [ZooAnimalData::class], version = 1 )
-abstract class ZooDataBase {
+abstract class ZooDataBase: RoomDatabase() {
     //DAO
     abstract fun zooDao(): ZooDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ZooDataBase? = null
+
+        fun getDataBase(context: Context): ZooDataBase {
+            //Si ya existe una instancia, se retoma
+            return INSTANCE?: synchronized(this){
+
+                //si no existe, la crea
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ZooDataBase::class.java,
+                    "zoo_dataBase"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
 }
